ac48264c9a78f12cbe72fa11fa109b8f4fe6457f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-07-27 11:54:10 +0600

    13_setting up vue js_s2

---
+++ Project 3 - Todo List - Initial/scripts.js
@@ -0,0 +1,27 @@
+
+var todos = [
+    {
+        text: 'Learn HTML, CSS and Javascript',
+        done: true
+
+    },
+    {
+        text: 'Learn the basics of Vue JS',
+        done: false
+
+    }
+
+
+];
+
+const todosApp = {
+   data(){
+    return {
+        todos: window.todos
+
+    }
+
+   }
+};
+
+Vue.createApp(todosApp).mount('#app');
